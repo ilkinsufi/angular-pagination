762a4934f0007fdafbd125bfefec4b8329ee8fdf
ilkinsufi <ilkinsufi@gmail.com>
2024-04-10 01:43:56 +0400

    fetching project from server (services & endpoints)

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-
-  constructor() { }
+  constructor(private apiService: ApiService) {}
+  getProducts = (url: string, params: any): Observable<any> => {
+    return this.apiService.get(url, params);
+  };
 }
