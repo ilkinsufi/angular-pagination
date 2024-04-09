2fec25b760cd18a662a77d2762089617fc06aa70
ilkinsufi <ilkinsufi@gmail.com>
2024-04-10 02:34:04 +0400

    getProducts function works

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Product, Products } from '../../types';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit() {
+    this.productsService
+      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .subscribe((products: Products) => {
+        console.log(products.items);
+      });
+  }
 }
